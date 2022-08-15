e62ef907bc95bcfacd4fb8da3907fdfeda5b925b
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2022-08-15 15:08:49 -0700

    add some timer tests

--- cfdp-core/src/timer.rs
+++ cfdp-core/src/timer.rs
@@ -50,11 +50,73 @@ impl Timer {
         }
     }
 
-    pub fn limit_reached(&self) -> bool {
-        *self.count.lock().unwrap() == self.max_count
+    pub fn get_count(&self) -> u32 {
+        *self.count.lock().unwrap()
     }
 
-    pub fn timout_occured(&self) -> bool {
+    pub fn limit_reached(&self) -> bool {
+        self.get_count() == self.max_count
+    }
+
+    pub fn timeout_occured(&self) -> bool {
         *self.occurred.lock().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use std::{thread, time::Duration};
+
+    #[test]
+    fn timeout() {
+        let mut timer = Timer::new(1_i64, 5);
+        timer.restart();
+        assert!(timer.guard.is_some());
+        thread::sleep(Duration::from_secs_f32(2.5_f32));
+        timer.pause();
+        assert_eq!(timer.get_count(), 2);
+        assert!(!timer.limit_reached());
+        assert!(timer.timeout_occured())
+    }
+
+    #[test]
+    fn no_timeout() {
+        let mut timer = Timer::new(2_i64, 1);
+        timer.restart();
+        thread::sleep(Duration::from_secs_f32(1.1_f32));
+        timer.pause();
+        assert!(!timer.timeout_occured());
+        // sleep again but make sure to cross the threshold from the original time out
+        thread::sleep(Duration::from_secs_f32(1.5_f32));
+        assert!(!timer.timeout_occured());
+
+        assert_eq!(timer.get_count(), 0)
+    }
+
+    #[test]
+    fn limit() {
+        let mut timer = Timer::new(1_i64, 1);
+        timer.restart();
+        thread::sleep(Duration::from_secs_f32(1.5));
+        assert!(timer.limit_reached());
+        // make sure the clamping works right
+        thread::sleep(Duration::from_secs_f32(1.5));
+        assert!(timer.limit_reached());
+        assert_eq!(timer.get_count(), 1);
+    }
+
+    #[test]
+    fn restart_no_fail() {
+        let mut timer = Timer::new(2_i64, 3);
+        timer.restart();
+        thread::sleep(Duration::from_secs_f32(1.5));
+        assert!(!timer.timeout_occured());
+        timer.restart();
+        thread::sleep(Duration::from_secs_f32(2.2));
+        timer.pause();
+
+        assert!(timer.timeout_occured())
     }
 }

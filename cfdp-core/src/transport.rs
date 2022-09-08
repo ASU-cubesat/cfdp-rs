d82b0e87524a9fce20a03cc0e5ff6f663c65e4c3
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2022-09-08 10:28:54 -0700

    add a uart feature and a SerialPort impl for transport

--- cfdp-core/src/transport.rs
+++ cfdp-core/src/transport.rs
@@ -11,25 +11,55 @@ use std::{
 
 use crossbeam_channel::{Receiver, Sender};
 use log::error;
+#[cfg(feature = "uart")]
+use serialport::{Error as SerialError, SerialPort};
 
 use crate::pdu::{PDUEncode, VariableID, PDU};
+
+#[derive(Debug)]
+pub enum TransportError {
+    Io(IoError),
+    Serial(SerialError),
+}
+impl std::fmt::Display for TransportError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Io(error) => error.fmt(f),
+            Self::Serial(error) => error.fmt(f),
+        }
+    }
+}
+impl std::error::Error for TransportError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Io(source) => Some(source),
+            Self::Serial(source) => Some(source),
+        }
+    }
+}
+
+impl From<IoError> for TransportError {
+    fn from(err: IoError) -> Self {
+        Self::Io(err)
+    }
+}
+impl From<SerialError> for TransportError {
+    fn from(err: SerialError) -> Self {
+        Self::Serial(err)
+    }
+}
+
 /// Transports are designed to run in a thread in the background
 /// inside a [Daemon](crate::daemon::Daemon) process
 pub trait PDUTransport {
-    type Output;
     type Err;
 
     /// Verify underyling communication method is ready.
     fn is_ready(&self) -> bool;
 
-    // /// Connect to all remote sources.
-    // /// A configuration should be able to be derived from a str
-    // /// whether by implementing a deserialization or [FromStr](std::str::FromStr)
-    // fn configure(config: &str) -> Result<Self::Output, Self::Err>;
-
     /// Send input PDU to the remote
     /// The implementation must have a method to lookup an Entity's address from the ID
-    fn request(&self, destination: VariableID, pdu: PDU) -> Result<(), Self::Err>;
+    fn request(&mut self, destination: VariableID, pdu: PDU) -> Result<(), Self::Err>;
 
     /// Provides logic for listening for incoming PDUs and sending any outbound PDUs
 
@@ -40,7 +70,7 @@ pub trait PDUTransport {
     /// [Transaction](crate::transaction::Transaction) as necessary.
     /// The signal is used to indicate a shutdown operation was requested.
     fn pdu_handler(
-        &self,
+        &mut self,
         signal: Arc<AtomicBool>,
         sender: Sender<PDU>,
         recv: Receiver<(VariableID, PDU)>,
@@ -65,14 +95,13 @@ impl UdpTransport {
     }
 }
 impl PDUTransport for UdpTransport {
-    type Output = Self;
-    type Err = IoError;
+    type Err = TransportError;
 
     fn is_ready(&self) -> bool {
         self.socket.local_addr().is_ok()
     }
 
-    fn request(&self, destination: VariableID, pdu: PDU) -> Result<(), Self::Err> {
+    fn request(&mut self, destination: VariableID, pdu: PDU) -> Result<(), Self::Err> {
         self.entity_map
             .get(&destination)
             .ok_or_else(|| IoError::from(ErrorKind::AddrNotAvailable))
@@ -81,10 +110,11 @@ impl PDUTransport for UdpTransport {
                     .send_to(pdu.encode().as_slice(), addr)
                     .map(|_n| ())
             })
+            .map_err(TransportError::Io)
     }
 
     fn pdu_handler(
-        &self,
+        &mut self,
         signal: Arc<AtomicBool>,
         sender: Sender<PDU>,
         recv: Receiver<(VariableID, PDU)>,
@@ -99,7 +129,7 @@ impl PDUTransport for UdpTransport {
                             Ok(()) => {}
                             Err(error) => {
                                 error!("Transport found disconnect sending channel: {}", error);
-                                return Err(IoError::from(ErrorKind::ConnectionAborted));
+                                return Err(IoError::from(ErrorKind::ConnectionAborted).into());
                             }
                         };
                     }
@@ -116,7 +146,7 @@ impl PDUTransport for UdpTransport {
                 }
                 Err(e) => {
                     error!("encountered IO error: {e}");
-                    return Err(e);
+                    return Err(e.into());
                 }
             }
 
@@ -127,10 +157,69 @@ impl PDUTransport for UdpTransport {
                 }
                 Err(err @ crossbeam_channel::TryRecvError::Disconnected) => {
                     error!("Transport found disconnected channel: {}", err);
-                    return Err(IoError::from(ErrorKind::ConnectionAborted));
+                    return Err(IoError::from(ErrorKind::ConnectionAborted).into());
                 }
             };
         }
+        Ok(())
+    }
+}
+
+#[cfg(feature = "uart")]
+impl<T: SerialPort> PDUTransport for T {
+    type Err = SerialError;
+
+    fn is_ready(&self) -> bool {
+        true
+    }
+
+    fn request(&mut self, _destination: VariableID, pdu: PDU) -> Result<(), Self::Err> {
+        self.write_all(pdu.encode().as_slice())
+            .map_err(SerialError::from)
+    }
+
+    fn pdu_handler(
+        &mut self,
+        signal: Arc<AtomicBool>,
+        sender: Sender<PDU>,
+        recv: Receiver<(VariableID, PDU)>,
+        _buffer_size: usize,
+    ) -> Result<(), Self::Err> {
+        while !signal.load(Ordering::Relaxed) {
+            // if there is anything in the read channel
+            // read one PDU at a time
+            // This gives a chance to send too without blocking
+            // if incoming data is persistent
+            if self.bytes_to_read()? > 0 {
+                match PDU::decode(self) {
+                    Ok(pdu) => {
+                        match sender.send(pdu) {
+                            Ok(()) => {}
+                            Err(error) => {
+                                error!("Transport found disconnect sending channel: {}", error);
+                                return Err(IoError::from(ErrorKind::ConnectionAborted).into());
+                            }
+                        };
+                    }
+                    Err(error) => {
+                        error!("Error decoding PDU: {}", error);
+                        // might need to stop depending on the error.
+                        // some are recoverable though
+                    }
+                }
+            }
+            match recv.try_recv() {
+                Ok((_entity, pdu)) => self.request(_entity, pdu)?,
+                Err(crossbeam_channel::TryRecvError::Empty) => {
+                    // nothing to do here
+                }
+                Err(err @ crossbeam_channel::TryRecvError::Disconnected) => {
+                    error!("Transport found disconnected channel: {}", err);
+                    return Err(IoError::from(ErrorKind::ConnectionAborted).into());
+                }
+            };
+        }
+
         Ok(())
     }
 }

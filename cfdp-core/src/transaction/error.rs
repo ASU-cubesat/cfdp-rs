2d82b6808dfc739451c37bb4888fbf19dd51fe59
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2023-02-21 13:04:35 -0700

    some readability spacing for transaction error

--- cfdp-core/src/transaction/error.rs
+++ cfdp-core/src/transaction/error.rs
@@ -16,28 +16,40 @@ pub type TransactionResult<T> = Result<T, TransactionError>;
 pub enum TransactionError {
     #[error("FileStore error during Transaction: {0}")]
     FileStore(#[from] Box<FileStoreError>),
+
     #[error("Error Communicating with transport: {0}")]
     Transport(#[from] Box<SendError<(VariableID, PDU)>>),
+
     #[error("Error transfering UserMessage {0}")]
     UserMessage(#[from] Box<SendError<(TransactionID, TransmissionMode, Vec<MessageToUser>)>>),
+
     #[error("No open file in transaction: {0:?}")]
     NoFile(TransactionID),
+
     #[error("Error during daemon thread listening: {0}")]
     Daemon(String),
+
     #[error("Thread lock poisoining.")]
     Poison,
+
     #[error("Error converting from integer: {0}")]
     IntConverstion(#[from] TryFromIntError),
+
     #[error("Transaction (ID: {0:?}, Mode: {1:?}) received unexpected PDU {2:}.")]
     UnexpectedPDU(TransactionSeqNum, TransmissionMode, String),
+
     #[error("Metadata missing for transaction: {0:?}.")]
     MissingMetadata(TransactionID),
+
     #[error("No NAKs present. Cannot send missing data.")]
     MissingNak,
+
     #[error("No Checksum received. Cannot verify file integrity.")]
     NoChecksum,
+
     #[error("Unable to send report to Daemon process. {0:?}.")]
     Report(#[from] SendError<Report>),
+
     #[error("No Transaction status for code {0}.")]
     InvalidStatus(u8),
 }

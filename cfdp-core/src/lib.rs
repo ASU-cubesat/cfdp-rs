477a4ca22efdc42b2acfefb902316f8355d52b6b
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2023-11-30 11:10:00 -0700

    add docs for daemon in core crate

--- cfdp-core/src/lib.rs
+++ cfdp-core/src/lib.rs
@@ -1,7 +1,13 @@
+/// Daemon related configurations and all [UserPrimitives](crate::daemon::UserPrimitive)
 pub mod daemon;
+
+/// Trait for interacting with a custom filestore and a default native implementation.
 pub mod filestore;
+
+/// All pdu definitions.
 pub mod pdu;
 
+/// Transaction related configurations and [Metadata](crate::transaction::Metadata)
 pub mod transaction;
 
 // re-exported for convenience and compatibility with the Primitives.
